2615ca5781f1c8b69d56563c379c5aac2f282733
Divnoor <divnoorsingh4602@gmail.com>
2025-01-24 12:40:21 -0800

    Enhance CreativeButtons component with arrow animation and improved button scaling effects

--- components/creative-buttons/CreativeButtons.tsx
+++ components/creative-buttons/CreativeButtons.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { motion } from "motion/react";
+import { ArrowRight } from "lucide-react";
 
 const CreativeButtons = () => {
   return (
@@ -32,6 +33,11 @@ const PushButton = () => {
       y: 0,
       scale: 1,
     },
+  };
+
+  const arrowVariants = {
+    initial: {},
+    hovered: {},
   };
 
   return (
@@ -78,29 +84,46 @@ const PushButton = () => {
 const AboutButton = () => {
   const bgVariants = {
     initial: {
-      scale: 0.05,
+      scale: 0.02,
     },
     hovered: {
       scale: 1,
     },
   };
 
+  const textVariants = {
+    initial: { x: 0 },
+    hovered: { x: "-20%", color: "white" },
+  };
+
   return (
     <>
       <motion.div
-        className="rounded-[28px] bg-white relative overflow-hidden flex items-center gap-4 w-[160px] h-[55px] justify-center"
+        className="rounded-[28px] bg-white relative  flex items-center gap-4 w-[160px] h-[55px] justify-center overflow-hidden"
         initial="initial"
         whileHover={"hovered"}
       >
+        {/* backgorund animation */}
         <motion.div
-          className="bg-blue-800 rounded-full size-[200px] absolute"
+          className="bg-blue-800 rounded-full size-[400px] absolute -left-[170px] "
           variants={bgVariants}
           transition={{
             type: "spring",
             bounce: 0.1,
-            duration: 0.25,
+            duration: 0.5,
           }}
         />
+        {/* text translate animation */}
+        <motion.div
+          className="uppercase tracking-tighter font-bold text-xl absolute left-[40px]"
+          variants={textVariants}
+        >
+          ABOUT US
+        </motion.div>
+        {/* arrow animation */}
+        <motion.div className="absolute right-[11px]">
+          <ArrowRight className="text-black" />
+        </motion.div>
       </motion.div>
     </>
   );
